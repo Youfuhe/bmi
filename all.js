e0d56477c05e916f90009647abeac363757ea532
Youfuhe <40340135@gm.nfu.edu.tw>
2019-08-05 19:26:15 +0800

    0805

--- all.js
+++ all.js
@@ -1,7 +1,8 @@
 var btn = $('.result');
-var restBtn = $('.green input');
+
+var restBtn = $('.bt');
 var bmi = 0;
-var data =  JSON.parse(localStorage.getItem('dataKey')) ||[];
+var data = JSON.parse(localStorage.getItem('dataKey')) || [];
 // var data =[];
 var deleteBtn = $('.content1')[0];
 
@@ -11,8 +12,8 @@ updateList(data);
 function updateList(data) {
     var str = '';
     for (i = 0; i < data.length; i++) {
-        str += 
-        `<div class="content">
+        str +=
+            `<div class="content">
             <div class="smallBox" style="background-color: ${data[i].color} "></div>
                 <h1> ${data[i].ch} </h1>
                     <div class="contentValue">
@@ -27,22 +28,26 @@ function updateList(data) {
     $('.record .content1').html(str);
     // $('.record .smallBox').css('background-color', data[i].color);
     localStorage.setItem('dataKey', JSON.stringify(data));
+
+    console.log(deleteBtn)
 };
 
 //刪除紀錄
+deleteBtn.addEventListener('click', dataDelete);
 
-deleteBtn.addEventListener('click',dataDelete);
 
-function dataDelete(e){
-    var index =  e.target.dataset.index;
+function dataDelete(e) {
+    e.preventDefault();
+    var index = e.target.dataset.index;
     // console.log(index);
-    if(e.target.tagName ==! 'A'){return};
-    data.splice(index,1);
-    localStorage.setItem('dataKey',JSON.stringify(data));
+    if (e.target.tagName !== 'A') { return };
+    console.log(index)
+    data.splice(index, 1);
+    localStorage.setItem('dataKey', JSON.stringify(data));
     updateList(data);
 }
 
-btn.click(function () {
+btn.click(function() {
     var heightValue = Number($('.heightValue').val());
     var weightValue = Number($('.weightValue').val());
     bmi = weightValue / ((heightValue / 100) * (heightValue / 100));
@@ -52,73 +57,84 @@ btn.click(function () {
     //輸入框=0或空白
     if (heightValue <= 0 || weightValue <= 0) {
         alert('輸入錯誤');
+        return;
     }
 
     if (bmi < 18.5) {
         //過輕
         $('.result').css('display', 'none');
+        $('.bt').css('display', 'unset');
         $('.green p').html(bmi);
         $('.green').css('display', 'flex');
         $('.green').css('border-color', '#31BAF9');
         $('.green').css('color', '#31BAF9');
-        $('.green .img').css('background-color', '#31BAF9');
-        $('.green h1').css('right', '-90px');
-        $('.green h1').html('過輕');
+        $('.bt h1').css('color', '#31BAF9');
+        $('.bt h1').html('過輕');
     } else if (bmi >= 18.5 && bmi < 24) {
         //理想
         $('.result').css('display', 'none');
+        $('.bt').css('display', 'unset');
+
         $('.green p').html(bmi);
         $('.green').css('display', 'flex');
         $('.green').css('border-color', '#86D73E');
         $('.green').css('color', '#86D73E');
-        $('.green .img').css('background-color', '#86D73E');
-        $('.green h1').css('right', '-90px');
-        $('.green h1').html('理想');
+
+        $('.bt h1').css('color', '#86D73E');
+        $('.bt h1').html('理想');
     } else if (24 <= bmi && bmi < 27) {
         //過重
         $('.result').css('display', 'none');
+        $('.bt').css('display', 'unset');
+
         $('.green p').html(bmi);
         $('.green').css('display', 'flex');
         $('.green').css('border-color', '#FF982D');
         $('.green').css('color', '#FF982D');
-        $('.green .img').css('background-color', '#FF982D');
-        $('.green h1').css('right', '-90px');
-        $('.green h1').html('過重');
+
+        $('.bt h1').css('color', '#FF982D');
+        $('.bt h1').html('過重');
     } else if (27 <= bmi && bmi < 30) {
         //輕度肥胖
         $('.result').css('display', 'none');
+        $('.bt').css('display', 'unset');
+
         $('.green p').html(bmi);
         $('.green').css('display', 'flex');
         $('.green').css('border-color', '#FF6C02');
         $('.green').css('color', '#FF6C02');
-        $('.green .img').css('background-color', '#FF6C02');
-        $('.green h1').css('right', '-150px');
-        $('.green h1').html('輕度肥胖');
+
+        $('.bt h1').css('color', '#FF6C02');
+        $('.bt h1').html('輕度肥胖');
     } else if (30 <= bmi && bmi < 35) {
         //中度肥胖
         $('.result').css('display', 'none');
+        $('.bt').css('display', 'unset');
+
         $('.green p').html(bmi);
         $('.green').css('display', 'flex');
         $('.green').css('border-color', '#FF6C02');
         $('.green').css('color', '#FF6C02');
-        $('.green .img').css('background-color', '#FF6C02');
-        $('.green h1').css('right', '-150px');
-        $('.green h1').html('中度肥胖');
+
+        $('.bt h1').css('color', '#FF6C02');
+        $('.bt h1').html('中度肥胖');
     } else if (bmi >= 35) {
         //重度肥胖
         $('.result').css('display', 'none');
+        $('.bt').css('display', 'unset');
+
         $('.green p').html(bmi);
         $('.green').css('display', 'flex');
         $('.green').css('border-color', '#FF1200');
         $('.green').css('color', '#FF1200')
-        $('.green .img').css('background-color', '#FF1200');
-        $('.green h1').css('right', '-150px');
-        $('.green h1').html('重度肥胖');
+
+        $('.bt h1').css('color', '#FF1200');
+        $('.bt h1').html('重度肥胖');
     }
 
     var date = new Date();
     todo = {
-        ch: $('.green h1').text(),
+        ch: $('.bt h1').text(),
         BMI: bmi,
         weigth: weightValue,
         height: heightValue,
@@ -137,5 +153,6 @@ restBtn.click(function reset() {
     bmi = 0;
     $('.result').css('display', 'flex');
     $('.green').css('display', 'none');
-});
 
+    $('.bt').css('display', 'none');
+});
